f151d4451af8f81123191db9524ae1c3cb649a6c
unknown <Muhammadkabeer2003@gmail.com>
2025-07-27 09:39:44 +0500

    Added Missing Features Section in Portfolio Management Page

--- src/app/(pages)/portfolio-management/page.tsx
+++ src/app/(pages)/portfolio-management/page.tsx
@@ -1,7 +1,9 @@
 import HeroSection from "@/src/components/ui/HeroSection";
 import HowItWorksSection from "@/src/components/ui/HowItWorksSection";
 import InvestmentSection from "@/src/components/portfolio/Investments";
-const features = [
+
+import ThreeColumnCards from "@/src/components/swap/ThreeColumnCards";
+const steps = [
   {
     number: "1.",
     text: "Sync your wallet to automatically fetch portfolio data.",
@@ -19,6 +21,28 @@ const features = [
     text: "Monitor asset trends and optimize your investments.",
   },
 ];
+const features = [
+  {
+    title: "Real-Time Dashboard",
+    description:
+      "View your total balance, gains/losses, and individual holdings at a glance",
+    icon: "/icons/mobile.svg",
+  },
+  {
+    title: "Detailed Analytics",
+    description:
+      "Dive into charts and historical data for each asset to fine-tune your strategy",
+    icon: "/icons/trend-up.svg",
+    mobileIcon: "/icons/trend-up-mobile.svg",
+  },
+  {
+    title: "Alerts & Reports",
+    description:
+      "Set custom price alerts and export portfolio reports to stay informed of market movements",
+    icon: "/icons/chart.svg",
+    mobileIcon: "/icons/chart-mobile.svg",
+  },
+];
 export default function PortfolioManagementPage() {
   return (
     <>
@@ -34,12 +58,13 @@ export default function PortfolioManagementPage() {
         description="Track, analyze, and optimize your crypto holdings with ease—here’s how to make the most of your portfolio"
         imageSrc="/icons/iPhones/portfolio-iphone.svg"
         imagePosition="left"
-        steps={features}
+        steps={steps}
         imageClasses="w-full h-full"
         isDark={false}
         ellipseClassName="bottom-[-320px] min-[400px]:bottom-[-380px] min-[500px]:bottom-[-510px] lg:bottom-[-420px] 2xl:bottom-[-480px] center-x-1/2 left-[50px]"
       />
       <InvestmentSection />
+      <ThreeColumnCards features={features} />
     </>
   );
 }
